5dd675078bc83698ef63956702e2dbca30651794
onyebeadi <48499547+onyebeadi@users.noreply.github.com>
2020-07-21 10:06:00 +0100

    added a try and catch block to handle guzzle exception

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Exception\GuzzleException;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use GuzzleHttp\Client;
 use App\Traits\ApiResponser;
@@ -13,12 +14,18 @@ class GuzzleController extends BaseController
     public function getData($id){
         $client = new Client(['headers'=>['content-type'=>'application/json' ,'Accept' => 'application/json' ],]);
         if($id != 1){
-            return $this->errorResponse('Invalid Index', 404);
+
+                return $this->errorResponse('Invalid Index', 404);
         }
-        $response = $client->request('GET','https://petstore.swagger.io/v2/pet/'.$id);
-        $data = $response->getBody();
-        $data = json_decode($data);
-        return $this->successResponse($data);
+        try {
+            $response = $client->request('GET', 'https://petstore.swagger.io/v2/pet/' . $id);
+            $data = $response->getBody();
+            $data = json_decode($data);
+            return $this->successResponse($data);
+        } catch (GuzzleException $e) {
+            return $this->errorResponse($e->getMessage(),$e->getCode());
+        }
+
     }
 
     public function postData(){
